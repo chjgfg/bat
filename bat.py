fb7f7f2f9efbde8a6469cd70aab3a0b1a96498fa
lyf <44043585+chjgfg@users.noreply.github.com>
2022-03-05 13:44:11 +0800

    Add files via upload

--- bat.py
+++ bat.py
@@ -2,3 +2,8 @@ import  streamlit as st
 
 x = st.slider("select a value")
 st.write(x,"squared is", x *x )
+name = st.text_input('Name')
+if not name:
+  st.warning('Please input a name.')
+  st.stop()
+st.success('Thank you for inputting a name.')
